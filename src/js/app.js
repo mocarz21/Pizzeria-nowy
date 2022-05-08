c7c4997f3912a6d3a134ce9743e9038a78fb6b10
Adam Mocarski <adammocarski22@gmail.com>
2022-05-08 17:17:20 +0200

    Add dynamic pages order, booking

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,45 @@ import { settings, select, classNames, templates } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages =document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks =document.querySelectorAll(select.nav.links);
+    thisApp.activatePages(thisApp.pages[0].id);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /*get page id from href atribute */
+        const id = clickedElement.getAttribute('href').replace('#',''); //zrobiłem wczesniej link zamiast clcickedElemet i działało identycznie Czy to robi jakąś różnice ?
+        console.log('id',id);
+        /* run thisApp.activatePage  with that id*/
+        thisApp.activatePages(id);
+      });
+    }
+  },
+  activatePages: function(pageId){
+    const thisApp = this;
+    /*add class 'active' to matching pages remove from non-matchng*/
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active,page.id == pageId);  //spowoduje wykonanie tego samego co jest zakomentowane poniżej
+    }
+    //  if(page.id == pageId){
+    //    page.classList.add(classNames.pages.active);
+    //  }else{
+    //    page.classList.remove(classNames.pages.active);
+    //  }
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+    /*add class 'active' to matching LINK remove from non-matchng*/
+  },
   initMenu: function(){
     const thisApp = this;
     
@@ -49,7 +88,7 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
-
+    thisApp.initPages();
     thisApp.initData();                            
     
     thisApp.initCart();
--- src/js/settings.js
+++ src/js/settings.js
@@ -2,10 +2,14 @@ export const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product',
+    bookingWidget: '#template-booking-widget',
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper'
+
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -26,6 +30,23 @@ export const select = {
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
     },
+    datePicker: {
+      wrapper: '.date-picker',
+      input: `input[name="date"]`,
+    },
+    hourPicker: {
+      wrapper: '.hour-picker',
+      input: 'input[type="range"]',
+      output: '.output',
+    },
+  },
+  booking: {
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+  },
+  nav: {
+    links: '.main-nav a',
   },
   cart: {
     productList: '.cart__order-summary',
@@ -55,6 +76,17 @@ export const classNames = {
   cart: {
     wrapperActive: 'active',
   },
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  }
+
 };
 
 export const settings = {
@@ -62,6 +94,13 @@ export const settings = {
     url: '//localhost:3131',
     products: 'products',
     orders: 'orders',
+    booking: 'booking',
+    event: 'event',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
+
   },
   amountWidget: {
     defaultValue: 1,
@@ -71,9 +110,22 @@ export const settings = {
   cart: {
     defaultDeliveryFee: 20,
   },
+  hours: {
+    open: 12,
+    close: 24,
+  },
+  datePicker: {
+    maxDaysInFuture: 14,
+  },
+  booking: {
+    tableIdAttribute: 'data-table',
+  },
 };
 
 export const templates = {
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
 };
+
+
